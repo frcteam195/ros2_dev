103722becafed3de3da5e7887951df110d0c7fcf
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-07-05 01:36:11 +0000

    UpdatePhoenixStuff

--- debug.sh
+++ debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROBOT_FOLDER="$(cd ${SCRIPT_DIR}/../*_Robot && pwd)"
-source "${ROBOT_FOLDER}/catkin_ws/devel/setup.bash"
+source "${ROBOT_FOLDER}/ros2_ws/install/setup.bash"
 export ROS_IP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 export ROS_MASTER_URI=http://10.1.95.5:11311
